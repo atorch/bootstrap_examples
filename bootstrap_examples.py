99ab674e8c326438aac7d33d1719fb67b14a9155
Adrian Torchiana <adrian.torchiana@gmail.com>
2021-07-29 14:34:35 -0700

    WIP on elastic net coefficients

--- bootstrap_examples.py
+++ bootstrap_examples.py
@@ -1,6 +1,16 @@
 import numpy as np
+import pandas as pd
+from sklearn.linear_model import ElasticNetCV
 
 np.random.seed(998877)
+
+
+def resample(dataset):
+
+    if isinstance(dataset, pd.DataFrame):
+        return dataset.sample(frac=1.0, replace=True)
+
+    return np.random.choice(dataset, size=dataset.size, replace=True)
 
 
 def bootstrap(dataset, estimator, n_bootstrap_samples):
@@ -9,8 +19,8 @@ def bootstrap(dataset, estimator, n_bootstrap_samples):
 
     for _ in range(n_bootstrap_samples):
 
-        # Note that we sample _with replacement_
-        resampled_dataset = np.random.choice(dataset, size=dataset.size, replace=True)
+        # Note that we sample *with* replacement
+        resampled_dataset = resample(dataset)
         values.append(estimator(resampled_dataset))
 
     return values
@@ -47,7 +57,7 @@ def example_1(n_obs=55, verbose=True):
 
 
 def example_2(n_replications=250):
-    """How often do the boostrap percentile CIs from example 1 actually contain the true median?
+    """How often do the bootstrap percentile CIs from example 1 actually contain the true median?
     """
 
     ci_contains_true_median = []
@@ -65,10 +75,58 @@ def example_2(n_replications=250):
     print(f"CI coverage: {coverage} (this should be close to 0.95)")
 
 
+def simulate_dataframe(n_obs):
+
+    df = pd.DataFrame({"x1": np.random.uniform(size=n_obs), "x2": np.random.uniform(size=n_obs)})
+
+    df["x3"] = df["x2"] + np.random.uniform(size=n_obs)
+
+    df["x4"] = np.random.normal(size=n_obs)
+    df["x5"] = df["x4"] + np.random.normal(size=n_obs, scale=2.0)
+
+    df["epsilon"] = np.random.normal(size=n_obs)
+
+    df["y"] = 10 + 5 * df["x1"] - 3 * df["x2"] + 1 * df["x3"] - 4 * df["x5"] + df["epsilon"]
+
+    return df
+
+
+def get_model_coefficients(df):
+
+    model = ElasticNetCV(n_alphas=10, cv=5, l1_ratio=0.9)
+
+    predictors = ["x1", "x2", "x3", "x4", "x5"]
+
+    model.fit(X=df[predictors], y=df["y"])
+
+    return model.coef_
+
+
+def example_3(n_obs=250):
+
+    """Based in part on Statistical Learning with Sparsity, Chapter 6
+
+    See 6.2 The Bootstrap, and especially Figure 6.4
+    """
+
+    df = simulate_dataframe(n_obs)
+
+    coef = get_model_coefficients(df)
+
+    coefs = np.array(bootstrap(df, get_model_coefficients, n_bootstrap_samples=3))
+
+    # TODO Plot
+    # TODO How often is each coef zeroed out?
+
+
 def main():
 
-    example_1()
-    example_2()
+    # example_1()
+    # example_2()
+
+    # TODO Another example where bootstrap doesn't work well (estimating the max, for example)
+
+    example_3()
 
 
 if __name__ == "__main__":
